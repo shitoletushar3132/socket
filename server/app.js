37314bff4ae23e65b7b91552cbb1cf1392b34780
sangam5756 <sangammunde3@gmail.com>
2024-12-28 16:21:04 +0530

    backend and frontend url changed

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const server = new createServer(app);
 // socket  created /also cors because server and client on different origin
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://chat-app12-nu.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
